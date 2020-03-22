1b34cd183c268572518126bbadf228a9b76f9e1b
Sergio Perez <perezsergio97.sp@gmail.com>
2020-03-21 23:23:11 -0500

    final style change

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -15,11 +15,11 @@ function Contact() {
                 <div className="row articlecontent">
                     <div className="col-md">
                         <form>
-                            Name: <br />
+                            <h4 className="text-white">Name:</h4>
                             <input className="display" type="text" name="name" placeholder="Your Name" /><br />
-                            Email: <br />
+                            <h4 className="text-white">Email:</h4>
                             <input className="display" type="text" name="email" placeholder="example@gmail.com" /><br />
-                            Message: <br />
+                            <h4 className="text-white" >Message:</h4>
                             <textarea className="display" type="text" name="name" placeholder="Write Message here" ></textarea>
                             </form>
                             <br />

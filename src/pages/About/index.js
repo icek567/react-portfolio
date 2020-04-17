5cdce89cc747b39a370bd3bc70097c18c4309250
Sergio Perez <perezsergio97.sp@gmail.com>
2020-04-17 18:11:59 -0500

    cleaning code, formatting

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -4,8 +4,9 @@ import me from "./me.jpg"
 
 function About() {
     return (
-        <div className="col-md-8 articlebox">
-          <div className="container">
+      <div className="col-md-8 articlebox">
+        <div className="container">
+          <div className="box2">
             <div className="row">
               <div className="col">
                 <h1 className="articletitle">About me</h1>
@@ -14,29 +15,30 @@ function About() {
 
             <div className="row articlecontent">
               <div className="col-md-4">
-                <img className="Sergiopic" src={me} alt="Sergio" width="200" height="300" class="img-thumbnail"/>
+                <img className="Sergiopic" src={me} alt="Sergio" width="200" height="300" class="img-thumbnail" />
               </div>
 
               <div className="col-md-8">
                 <p className="text-white"> Hello everyone! My name is Sergio Perez and I am a student enrolled in Coding Boot Camp at
-          Northwestern. When I am not practicing my coding skills you can catch me at my job at Northshore Evanston
-          Hospital. There I work as a Patient Transporter, basically if
-          a patient needs to go to a test site, I am the one they call.
-          I enjoy taking pictures of my girlfriends dog Nikki, as well as pictures landscapes with my little sister. I
-          also love watching Game of Thrones, I am currently on season 6, so no spoilers please.
-          My favorite food is my mom's homemade Mexican food and my favorite drink is water.
+                Northwestern. When I am not practicing my coding skills you can catch me at my job at Northshore Evanston
+                Hospital. There I work as a Patient Transporter, basically if
+                a patient needs to go to a test site, I am the one they call.
+                I enjoy taking pictures of my girlfriends dog Nikki, as well as pictures landscapes with my little sister. I
+                also love watching Game of Thrones, I am currently on season 6, so no spoilers please.
+                My favorite food is my mom's homemade Mexican food and my favorite drink is water.
           My favorite place to visit, besides Mexico, is Sedona, Arizona. It reminds me of my parents’ hometown.</p>
-          
-          <p className="text text-white">When I am not at work or class I like to spend time with my family and loved ones. Some
-            other things that I like to do are traveling, hiking, playing video games, going
-            swimming and hiking. Many people have always said that I come off as shy at first but that is just me getting
-            comfortable to the people around me. Once I do I go all out me showing my good sence of humor and very talkative
+
+                <p className="text text-white">When I am not at work or class I like to spend time with my family and loved ones. Some
+                other things that I like to do are traveling, hiking, playing video games, going
+                swimming and hiking. Many people have always said that I come off as shy at first but that is just me getting
+                comfortable to the people around me. Once I do I go all out me showing my good sence of humor and very talkative
             person that I am.</p>
               </div>
             </div>
 
           </div>
         </div>
+      </div>
     )
 }
 
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,30 +1,43 @@
 import React from "react";
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function Contact() {
     return (
         <div className="col-md-8 articlebox">
             <div className="container">
-                <div className="row">
-                    <div className="col">
-                    <h1 className="articletitle">Contact</h1>
+                <div className="box2">
+                    <div className="row">
+                        <div className="col">
+                            <h1 className="articletitle">Contact</h1>
+                        </div>
+
                     </div>
 
-                </div>
-
-                <div className="row articlecontent">
-                    <div className="col-md">
-                        <form>
-                            <h4 className="text-white">Name:</h4>
-                            <input className="display" type="text" name="name" placeholder="Your Name" /><br />
-                            <h4 className="text-white">Email:</h4>
-                            <input className="display" type="text" name="email" placeholder="example@gmail.com" /><br />
-                            <h4 className="text-white" >Message:</h4>
-                            <textarea className="display" type="text" name="name" placeholder="Write Message here" ></textarea>
+                    <div className="row articlecontent">
+                        <div className="col-md">
+                            <form>
+                                <h4 className="text-white">Name:</h4>
+                                <input className="display2" type="text" name="name" placeholder="Your Name" /><br />
+                                <h4 className="text-white">Email:</h4>
+                                <input className="display2" type="text" name="email" placeholder="example@gmail.com" /><br />
+                                <h4 className="text-white" >Message:</h4>
+                                <textarea className="display" type="text" name="name" placeholder="Write Message here" ></textarea>
                             </form>
                             <br />
-                            <button className="button" type="submit" value="Submit" >Submit</button>
-                            <br/>
+                            <button className="btn btn-outline-light"  type="submit" value="Submit" >Submit</button>
+                            <br />
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h2 class="articletitle">Contact Info</h2>
+                            <ul>
+                                <li class="text-light"><strong>Email:</strong> <a href="/">sergio_perezg@yahoo.com</a></li>
+                                <li class="text-light"><strong>Github:</strong> <a href="https://github.com/icek567">icek567</a></li>
+                                <li class="text-light"><strong>Portfolio:</strong> <Link to="/portfolio">Check it Out!</Link></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>

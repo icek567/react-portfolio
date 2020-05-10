f357f498d872953c691cedf918d623852c78de3c
Sergio Perez <perezsergio97.sp@gmail.com>
2020-05-10 16:11:07 -0500

    added buttons to github, chnaged text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -19,20 +19,22 @@ function About() {
               </div>
 
               <div className="col-md-8">
-                <p className="text-white"> Hello everyone! My name is Sergio Perez and I am a student enrolled in Coding Boot Camp at
-                Northwestern. When I am not practicing my coding skills you can catch me at my job at Northshore Evanston
+                <p className="text-white">  Hello everyone! My name is Sergio Perez and I'm an aspiring Web Developer with a passion in Front End Design. When I am not practicing my coding skills you can catch me at my job at Northshore Evanston
                 Hospital. There I work as a Patient Transporter, basically if
                 a patient needs to go to a test site, I am the one they call.
-                I enjoy taking pictures of my girlfriends dog Nikki, as well as pictures landscapes with my little sister. I
-                also love watching Game of Thrones, I am currently on season 6, so no spoilers please.
+                I enjoy taking pictures with my camera(Sony Alpha 6000), preferably of landscapes.
                 My favorite food is my mom's homemade Mexican food and my favorite drink is water.
           My favorite place to visit, besides Mexico, is Sedona, Arizona. It reminds me of my parents’ hometown.</p>
 
-                <p className="text text-white">When I am not at work or class I like to spend time with my family and loved ones. Some
+                <p className="text-white">  When I am not at work I like to spend time with my family and loved ones. Some
                 other things that I like to do are traveling, hiking, playing video games, going
-                swimming and hiking. Many people have always said that I come off as shy at first but that is just me getting
+                swimming, playing soccer and watching sports. Many people have always said that I come off as shy at first but that is just me getting
                 comfortable to the people around me. Once I do I go all out me showing my good sence of humor and very talkative
-            person that I am.</p>
+                person that I am.</p>
+
+                <p className="text-white">  My strongest laguages in code I would say are React, Node.js, Javascript, Mongo and Mongoose. I am also very good with
+                  using the frameworks of Bootstrap, Materialize and CSS to add polishing touches.
+                </p>
               </div>
             </div>
 
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -28,6 +28,13 @@ function Portfolio() {
                                 “Keyword” or “Title”.</p>
                                     <Project name="Multi-Media-App" img={media} link="https://icek567.github.io/Multi-Media-Search-App/" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Multi-Media-Search-App" role="button">GitHub</a>
+                                <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>jquery</li>
+                                    <li>Javascript</li>
+                                    <li>Materialize</li>
+                                </ul>
                             </div>
                         </div>
                         <div className="col-sm-6">
@@ -39,6 +46,13 @@ function Portfolio() {
                             to them later. </p>
                                     <Project name="Schedule" img={Schedule} link="https://icek567.github.io/Schedule/" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Schedule" role="button">GitHub</a>
+                                <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>CSS</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -46,21 +60,37 @@ function Portfolio() {
                         <div className="col-sm-6">
                             <div className="card text-white bg-dark">
                                 <div className="card-body">
-                                    <h5 className="card-text">Superhero Quiz</h5>
+                                    <h5 className="card-title">Superhero Quiz</h5>
                                     <p className="card-text">How good do you know superhero lore? Take this quiz and find out if you know enough to pass.
                             See your high school but if you get too many wrong answers it will end the quiz early.</p>
                                     <Project name="Superhero Quiz" img={quiz} link="https://icek567.github.io/Quiz-2" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Superhero-Quiz" role="button">GitHub</a>
+                                <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>jquery</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
                             </div>
                         </div>
                         <div className="col-sm-6">
                             <div className="card text-white bg-dark">
                                 <div className="card-body">
-                                    <h5 className="card-text">Burger App</h5>
+                                    <h5 className="card-title">Burger App</h5>
                                     <p className="card-text">Devour the burger or toss it away, the choice is up to you. On the left side you have the burgers that have been created while on the right
                             are the ones that you ate.</p>
                                     <Project name="Burger App" img={burger} link="https://arcane-thicket-32283.herokuapp.com/" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/burger-app" role="button">GitHub</a>
+                                <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>Express</li>
+                                    <li>Express-handlebars</li>
+                                    <li>Bootstrap</li>
+                                    <li>MySQL</li>
+                                    <li>Heroku App</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
@@ -68,21 +98,35 @@ function Portfolio() {
                         <div className="col-sm-6">
                             <div className="card text-white bg-dark">
                                 <div className="card-body">
-                                    <h5 className="card-text">Password Generator</h5>
+                                    <h5 className="card-title">Password Generator</h5>
                                     <p className="card-text">Need to come up with a strong password for any online account? With this you are able to generate one for you and be able to copy it to clipboard. 
                                     This website uses pure javascript to generate from random symbols, numbers and letters.</p>
                                     <Project  name="Password Generator" img={password} link="https://icek567.github.io/Password-Generator/" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Password-Generator" role="button">GitHub</a>
+                               <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>HTML</li>
+                                    <li>Javascript</li>
+                                    <li>Bootstrap</li>
+                                </ul>
                             </div>
                         </div>
                         <div className="col-sm-6">
                             <div className="card text-white bg-dark">
                                 <div className="card-body">
-                                    <h5 className="card-text">Team Maker</h5>
+                                    <h5 className="card-title">Team Maker</h5>
                                     <p className="card-text">Using node.js, form your terminal you are able to create a team for yourself. Create the manager, Engineer and the intern.
                                      Once all of the information was put in, open the new file in your web browser and it will show you the website. </p>
                                     <Project name="Team Maker" img={team} link="https://github.com/icek567/Team-Maker" />
                                 </div>
+                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Team-Maker" role="button">GitHub</a>
+                                <h5 id="list">Technologies used:</h5>
+                                <ul>
+                                    <li>Node.js</li>
+                                    <li>Javascript</li>
+                                    <li>Inquirer</li>
+                                </ul>
                             </div>
                         </div>
                     </div>

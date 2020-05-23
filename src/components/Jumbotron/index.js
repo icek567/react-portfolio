55e68c74a7098bcebdb1e2435d5d22c9d13fff9e
Sergio Perez <perezsergio97.sp@gmail.com>
2020-05-23 17:10:44 -0500

    added jumbotron header

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron () {
+    return(
+        <div class="jumbotron jumbotron-fluid" id="jumbotron">
+        <div class="container">
+            <h1 class=" display-3 text-white text-center"> Sergio Perez</h1>
+            <h3 class="display-4 text-white text-center">Welcome to my Website</h3>
+        </div>
+    </div>
+    )
+    
+}
+
+export default Jumbotron;

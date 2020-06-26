7d08b3b11f8460a766e772f028643188a6390837
Sergio Perez <perezsergio97.sp@gmail.com>
2020-06-25 21:30:56 -0500

    carousel added

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,47 +1,97 @@
 import React from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
+import flower from "./flower.jpg";
+import river from "./river.jpg";
+import chivas from "./chivas.JPG";
+import night from "./night.JPG";
+import woods from "./woods.jpg";
+import birdhouse from "./birdhouse.jpg";
+import Carousel from 'react-bootstrap/Carousel';
 
 function Contact() {
     return (
-        <div className="col-md-8 articlebox">
-            <div className="container">
-                <div className="box2">
-                    <div className="row">
-                        <div className="col">
-                            <h1 className="articletitle">Contact</h1>
-                        </div>
 
-                    </div>
+        <Carousel>
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    src={flower}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3 className="information" >First slide label</h3>
+                    <p className="information" >Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    src={chivas}
+                    alt="Third slide"
+                />
 
-                    <div className="row articlecontent">
-                        <div className="col-md">
-                            <form>
-                                <h4 className="text-white">Name:</h4>
-                                <input className="display2" type="text" name="name" placeholder="Your Name" /><br />
-                                <h4 className="text-white">Email:</h4>
-                                <input className="display2" type="text" name="email" placeholder="example@gmail.com" /><br />
-                                <h4 className="text-white" >Message:</h4>
-                                <textarea className="display" type="text" name="name" placeholder="Write Message here" ></textarea>
-                            </form>
-                            <br />
-                            <button className="btn btn-outline-light"  type="submit" value="Submit" >Submit</button>
-                            <br />
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <h2 class="articletitle">Contact Info</h2>
-                            <ul>
-                                <li class="text-light"><strong>Email:</strong><a title="Email Me" href="mailto:sergio_perezg@yahoo.com">sergio_perezg@yahoo.com</a></li>
-                                <li class="text-light"><strong>Github:</strong> <a title="Chech it out!" href="https://github.com/icek567">icek567</a></li>
-                                <li class="text-light"><strong>Portfolio:</strong> <Link to="/portfolio" title ="My work">Check it Out!</Link></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    className="Carousel"
+                    src={river}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    src={night}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Fourth slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    src={woods}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Fifth slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+                <img
+                    height={500}
+                    width={700}
+                    src={birdhouse}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>Sixth slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     )
 }
 

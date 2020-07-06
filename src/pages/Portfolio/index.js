9de90a6e8cb1ff682f64bcfb2e9c3ea13ad79850
Sergio Perez <perezsergio97.sp@gmail.com>
2020-07-06 17:11:35 -0500

    added accordion to all, format document

--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -33,7 +33,6 @@ function Portfolio() {
                                         “Keyword” or “Title”.</p>
                                     </Card.Text>
                                     <Project name="Multi-Media" img={media} link="https://icek567.github.io/Multi-Media-Search-App/" />
-                                    <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Multi-Media-Search-App ">GitHub</Button>
                                     <Accordion>
                                         <Card classname="accordion" border="info" id="CardColor">
                                             <Card.Header id="point">
@@ -49,6 +48,7 @@ function Portfolio() {
                                                         <li>Javascript</li>
                                                         <li>Materialze</li>
                                                     </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Multi-Media-Search-App ">GitHub</Button>
                                                     </Card.Text>
                                                 </Card.Body>
                                             </Accordion.Collapse>
@@ -58,96 +58,174 @@ function Portfolio() {
                             </Card>
                         </div>
                         <div className="col-sm-6">
-                            <div className="card text-white" id="CardColor">
-                                <div className="card-body">
-                                    <h5 className="card-title">Day Schedule</h5>
-                                    <p className="card-text">This was a Day schedule that works for a regular morning shift. You are
-                                    able to type task inside of the boxes and saved them so you are able to exit and come back
-                            to them later. </p>
+                            <Card id="CardColor">
+                                <Card.Body>
+                                    <Card.Title><h5>Day Schedule</h5></Card.Title>
+                                    <Card.Text>
+                                        <p>This was a Day schedule that works for a regular morning shift. You are
+                                         able to type task inside of the boxes and saved them so you are able to exit and come back to them later. </p>
+                                    </Card.Text>
                                     <Project name="Schedule" img={Schedule} link="https://icek567.github.io/Schedule/" />
-                                </div>
-                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Schedule" role="button">GitHub</a>
-                                <h5 id="list">Technologies used:</h5>
-                                <ul>
-                                    <li>CSS</li>
-                                    <li>Javascript</li>
-                                    <li>Bootstrap</li>
-                                </ul>
-                            </div>
+                                    <Accordion>
+                                        <Card classname="accordion" border="info" id="CardColor">
+                                            <Card.Header id="point">
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    <h5>Technologies Used </h5>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <Card.Body >
+                                                    <Card.Text> <ul>
+                                                        <li>CSS</li>
+                                                        <li>Javascript</li>
+                                                        <li>Materialze</li>
+                                                    </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Schedule">GitHub</Button>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </Card.Body>
+                            </Card>
                         </div>
                     </div>
                     <div className="row articlecontent">
                         <div className="col-sm-6">
-                            <div className="card text-white" id="CardColor">
-                                <div className="card-body">
-                                    <h5 className="card-title">Superhero Quiz</h5>
-                                    <p className="card-text">How good do you know superhero lore? Take this quiz and find out if you know enough to pass.
-                            See your high school but if you get too many wrong answers it will end the quiz early.</p>
-                                    <Project name="Superhero Quiz" img={quiz} link="https://icek567.github.io/Quiz-2" />
-                                </div>
-                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Superhero-Quiz" role="button">GitHub</a>
-                                <h5 id="list">Technologies used:</h5>
-                                <ul>
-                                    <li>jquery</li>
-                                    <li>Javascript</li>
-                                    <li>Bootstrap</li>
-                                </ul>
-                            </div>
+                            <Card id="CardColor">
+                                <Card.Body>
+                                    <Card.Title><h5>SuperHero Quiz</h5></Card.Title>
+                                    <Card.Text>
+                                        <p className="card-text">How good do you know superhero lore? Take this quiz and find out if you know enough to pass.
+                                         See your high school but if you get too many wrong answers it will end the quiz early.</p>
+                                    </Card.Text>
+                                    <Project name="SuperHero Quiz" img={quiz} link="https://icek567.github.io/Superhero-Quiz/" />
+                                    <Accordion>
+                                        <Card classname="accordion" border="info" id="CardColor">
+                                            <Card.Header id="point">
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    <h5>Technologies Used </h5>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <Card.Body >
+                                                    <Card.Text>
+                                                        <ul>
+                                                            <li>jquery</li>
+                                                            <li>Javascript</li>
+                                                            <li>Bootstrap</li>
+                                                        </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Superhero-Quiz">GitHub</Button>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </Card.Body>
+                            </Card>
                         </div>
                         <div className="col-sm-6">
-                            <div className="card text-white" id="CardColor">
-                                <div className="card-body">
-                                    <h5 className="card-title">Burger App</h5>
-                                    <p className="card-text">Devour the burger or toss it away, the choice is up to you. On the left side you have the burgers that have been created while on the right
-                            are the ones that you ate.</p>
+                            <Card id="CardColor">
+                                <Card.Body>
+                                    <Card.Title><h5>Burger App</h5></Card.Title>
+                                    <Card.Text>
+                                        <p>Devour the burger or toss it away, the choice is up to you. On the left side you have the burgers that have been created while on the right are the ones that you ate.</p>
+                                    </Card.Text>
                                     <Project name="Burger App" img={burger} link="https://arcane-thicket-32283.herokuapp.com/" />
-                                </div>
-                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/burger-app" role="button">GitHub</a>
-                                <h5 id="list">Technologies used:</h5>
-                                <ul>
-                                    <li>Express</li>
-                                    <li>Express-handlebars</li>
-                                    <li>Bootstrap</li>
-                                    <li>MySQL</li>
-                                    <li>Heroku App</li>
-                                </ul>
-                            </div>
+                                    <Accordion>
+                                        <Card classname="accordion" border="info" id="CardColor">
+                                            <Card.Header id="point">
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    <h5>Technologies Used </h5>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <Card.Body >
+                                                    <Card.Text>
+                                                        <ul>
+                                                            <li>Express</li>
+                                                            <li>Express-handlebars</li>
+                                                            <li>Bootstrap</li>
+                                                            <li>MySQL</li>
+                                                            <li>Heroku App</li>
+                                                        </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/burger-app">GitHub</Button>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </Card.Body>
+                            </Card>
                         </div>
                     </div>
                     <div className="row articlecontent">
                         <div className="col-sm-6">
-                            <div className="card text-white" id="CardColor">
-                                <div className="card-body">
-                                    <h5 className="card-title">Password Generator</h5>
-                                    <p className="card-text">Need to come up with a strong password for any online account? With this you are able to generate one for you and be able to copy it to clipboard.
+                            <Card id="CardColor">
+                                <Card.Body>
+                                    <Card.Title><h5>Password Generator</h5></Card.Title>
+                                    <Card.Text>
+                                        <p>Need to come up with a strong password for any online account? With this you are able to generate one for you and be able to copy it to clipboard.
                                     This website uses pure javascript to generate from random symbols, numbers and letters.</p>
+                                    </Card.Text>
                                     <Project name="Password Generator" img={password} link="https://icek567.github.io/Password-Generator/" />
-                                </div>
-                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Password-Generator" role="button">GitHub</a>
-                                <h5 id="list">Technologies used:</h5>
-                                <ul>
-                                    <li>HTML</li>
-                                    <li>Javascript</li>
-                                    <li>Bootstrap</li>
-                                </ul>
-                            </div>
+                                    <Accordion>
+                                        <Card classname="accordion" border="info" id="CardColor">
+                                            <Card.Header id="point">
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    <h5>Technologies Used </h5>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <Card.Body >
+                                                    <Card.Text>
+                                                        <ul>
+                                                            <li>HTML5</li>
+                                                            <li>Javascript</li>
+                                                            <li>Bootstrap</li>
+                                                        </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Password-Generator">GitHub</Button>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </Card.Body>
+                            </Card>
                         </div>
                         <div className="col-sm-6">
-                            <div className="card text-white" id="CardColor">
-                                <div className="card-body">
-                                    <h5 className="card-title">Team Maker</h5>
-                                    <p className="card-text">Using node.js, form your terminal you are able to create a team for yourself. Create the manager, Engineer and the intern.
+                            <Card id="CardColor">
+                                <Card.Body>
+                                    <Card.Title><h5>Team Maker</h5></Card.Title>
+                                    <Card.Text>
+                                        <p> Using node.js, form your terminal you are able to create a team for yourself. Create the manager, Engineer and the intern.
                                      Once all of the information was put in, open the new file in your web browser and it will show you the website. </p>
+                                    </Card.Text>
                                     <Project name="Team Maker" img={team} link="https://github.com/icek567/Team-Maker" />
-                                </div>
-                                <a class="btn btn-outline-info btn-sm" id="btn" href="https://github.com/icek567/Team-Maker" role="button">GitHub</a>
-                                <h5 id="list">Technologies used:</h5>
-                                <ul>
-                                    <li>Node.js</li>
-                                    <li>Javascript</li>
-                                    <li>Inquirer</li>
-                                </ul>
-                            </div>
+                                    <Accordion>
+                                        <Card classname="accordion" border="info" id="CardColor">
+                                            <Card.Header id="point">
+                                                <Accordion.Toggle as={Card.Header} eventKey="0">
+                                                    <h5>Technologies Used </h5>
+                                                </Accordion.Toggle>
+                                            </Card.Header>
+                                            <Accordion.Collapse eventKey="0">
+                                                <Card.Body >
+                                                    <Card.Text>
+                                                        <ul>
+                                                            <li>HTML5</li>
+                                                            <li>Node.js</li>
+                                                            <li>Javascript</li>
+                                                            <li>Inquirer</li>
+                                                        </ul>
+                                                        <Button variant="btn btn-outline-info btn-sm" href="https://github.com/icek567/Team-Maker">GitHub</Button>
+                                                    </Card.Text>
+                                                </Card.Body>
+                                            </Accordion.Collapse>
+                                        </Card>
+                                    </Accordion>
+                                </Card.Body>
+                            </Card>
                         </div>
                     </div>
                 </div>
